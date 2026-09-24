efe99b07c1f671209b0233b2f886500ad78f2cfc
ninjared311 <stockbano@yahoo.com>
2026-09-23 23:51:11 -0400

    Delete sw 2.js

--- sw 2.js
+++
@@ -1,5 +0,0 @@
-const CACHE='yard-work-tracker-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request))));
